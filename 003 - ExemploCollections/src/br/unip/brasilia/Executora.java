315bfad865ad456f787cb19d0f2fcdb75617342d
diego <diego@macbook>
2021-03-15 11:19:29 -0300

    Add 003 project

---
+++ 003 - ExemploCollections/src/br/unip/brasilia/Executora.java
@@ -0,0 +1,46 @@
+/*
+	OBJETIVO: Apresentar o básico de Collections 
+	ENTRADA: N/A
+	SAÍDA: Variável de acordo com a Collection
+*/
+
+package br.unip.brasilia;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class Executora {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> al = new ArrayList<>();
+		//Imaginem o CRUD
+		
+		al.add("Adalberto");
+		al.add("Beatriz");
+		al.add("Carlos");
+		al.add("Diego");
+		al.add("Zoraia");
+		
+		System.out.println("Elementos do ArrayList:");
+		System.out.println(al);
+		
+		
+		LinkedList<String> ll = new LinkedList<>();
+		
+		ll.add("Carlos");
+		ll.add("Adalberto");
+		ll.add("Beatriz");
+		ll.add("Zoraia");
+		ll.add("Diego");
+		
+		System.out.println("Elementos do LinkedList:");
+		System.out.println(ll);
+
+	}
+
+}
