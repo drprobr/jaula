bc3d6af207fea6346b13bcf82875bc7ac34280ed
diego <diego@macbook>
2021-03-16 09:43:41 -0300

    Change 004 project

--- 004 - Multiplos MMs/src/Multiplos.java
+++ 004 - Multiplos MMs/src/Multiplos.java
@@ -4,35 +4,8 @@ class Multiplos {
 
     public static void main (String args[]){
     	
-    	Scanner ler = new Scanner(System.in);
+    	//Multiplos
     	
-    	int num = 0;
-    	
-    	while(num < 1 || num > 10) {
-        	System.out.print("Número: ");
-        	num = ler.nextInt();    		
-    	}
-    	
-    	for(int i=num;i<101;i++){
-
-    		if(i%15==0) {
-    			System.out.println("M15");
-    		}else {
-        		if(i%10==0) {
-        			System.out.println("M10");
-        		}else {
-            		if(i%5==0) {
-            			System.out.println("M5");
-            		}else {
-            			System.out.println(i);
-            		}
-        		}
-    		}
-    		
-    	}
-    	
-    	ler.close();
-    
     }
 }
 
